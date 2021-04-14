ac85dac4a86db089d530df5990c00812165a484c
tomomaru87 <tikksyou1129@gmail.com>
2021-04-14 09:37:14 +0900

    ログイン後提出者の情報のみを表示する。

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth; 
 
 class HomeController extends Controller
 {
@@ -15,11 +16,12 @@ class HomeController extends Controller
     }
 
     public function index(Request $request)
-    {
+    {   
+        $user = Auth::user();
         $items= User::orderby('created_at')->get();
-        return view('user.home')->with(['items'=>$items]);
+        return view('user.home')->with(['items'=>$items,'user'=>$user]);
 
-        
+
       
         
 
